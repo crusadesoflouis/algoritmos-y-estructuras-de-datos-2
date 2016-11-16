8a2be301474f31ed863fe84a6d74aa4ca04365a6
Daro <alejandrodecheverri@gmail.com>
2016-11-16 19:31:53 -0300

    Subiendo cambios en TiposJuegos.h - Alejandro

--- TiposJuego.h
+++ TiposJuego.h
@@ -5,13 +5,14 @@
 
 typedef unsigned int Jugador;
 typedef std::string Pokemon;
+
+
  
 struct Coordenada
 {
 	Nat latitud;
 	Nat longitud;
-	Coordenada(Nat latitud, Nat longitud) : latitud(latitud), longitud(longitud)
-	{
+	Coordenada(Nat latitud, Nat longitud) : latitud(latitud), longitud(longitud){
 	}
 };
 
@@ -22,5 +23,25 @@ bool operator == (const Coordenada& c1, const Coordenada& c2){
 bool operator != (const Coordenada& c1, const Coordenada& c2){
  return not (c1 == c2);
 }
+
+Nat cuadRestaLat (const Coordenada& c1 , const Coordenada& c2 ){
+		if (c1.latitud > c2.latitud)
+			return ( (c1.latitud - c2.latitud) ^ 2 );
+		else
+			return ( (c2.latitud - c1.latitud) ^ 2 );
+}
+
+Nat cuadRestaLong (const Coordenada& c1 , const Coordenada& c2 ){
+		if (c1.longitud > c2.longitud)
+			return ( (c1.longitud - c2.longitud) ^ 2 );
+		else
+			return ( (c2.longitud - c1.longitud) ^ 2 );
+}
+
+Nat distEuclidea(const Coordenada& c1 , const Coordenada& c2 ){
+		return cuadRestaLat(c1,c2) + cuadRestaLong(c1,c2);
+}
+
+
 
 #endif
